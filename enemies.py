3a04816456b4bafe8d4b51090868a6adb6c6c968
Monglettuce <WilsonCA5@cardiff.ac.uk>
2014-10-22 21:39:29 +0100

    Jarvis and Mebin changes to map and enemies

--- enemies.py
+++ enemies.py
@@ -18,7 +18,6 @@ enemy_giantspider = {
 
 #The numbers need to be changed so that it is relative to other parts of the game.
 
-
 enemy_goblin = {
 	"name" : "Goblin",
 
@@ -37,7 +36,6 @@ enemy_goblin = {
 
 #The numbers need to be changed so that it is relative to other parts of the game.
 
-
 enemy_chiefgoblin = {
 	"name" : "Chief Goblin",
 
@@ -55,7 +53,6 @@ enemy_chiefgoblin = {
 }
 
 #The numbers need to be changed so that it is relative to other parts of the game.
-
 
 enemy_knight = {
 	"name" : "Knight",
@@ -177,7 +174,7 @@ enemy_boar = {
 
 	"speed" : 10,
 
-	"reward" : "",
+	"reward" : armour_ironhelmet,
 
 	"money" : 40
 }
@@ -213,7 +210,7 @@ enemy_gnolls = {
 
 	"speed" : 5,
 
-	"reward" : "",
+	"reward" : armour_ironboots,
 
 	"money" : 45
 }
@@ -249,7 +246,7 @@ enemy_beastmen = {
 
 	"speed" : 10,
 
-	"reward" : "",
+	"reward" : armour_ironchestplate,
 
 	"money" : 30
 }
@@ -285,7 +282,7 @@ enemy_deadlyscorp = {
 
 	"speed" : 10,
 
-	"reward" : "",
+	"reward" : weapon_bow,
 
 	"money" : 50
 }
@@ -310,6 +307,533 @@ enemy_kraken = {
 
 #The numbers need to be changed so that it is relative to other parts of the game.
 
+enemy_ghoul = {
+	"name" : "Ghoul",
+
+	"health" : 30,
+
+	"strength" : 10,
+
+	"defence" : 50,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 50
+}
+
+enemy_orc = {
+	"name" : "Orc",
+
+	"health" : 50,
+
+	"strength" : 20,
+
+	"defence" : 50,
+
+	"speed" : 5,
+
+	"reward" : "",
+
+	"money" : 70
+}
+
+enemy_cyclops = {
+	"name" : "Cyclops",
+
+	"health" : 75,
+
+	"strength" : 70,
+
+	"defence" : 30,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 100
+}
+
+enemy_groupwolf = {
+	"name" : "Group of Hungry Wolfs",
+
+	"health" : 100,
+
+	"strength" : 50,
+
+	"defence" : 10,
+
+	"speed" : 80,
+
+	"reward" : "",
+
+	"money" : 100
+}
+
+enemy_ogres = {
+	"name" : "Ogres",
+
+	"health" : 70,
+
+	"strength" : 50,
+
+	"defence" : 20,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 75
+}
+
+enemy_babyminotaurs = {
+	"name" : "Baby Minotaurs",
+
+	"health" : 10,
+
+	"strength" : 20,
+
+	"defence" : 10,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 10
+}
+
+enemy_chiefminotaurs = {
+	"name" : "Chief Minotaurs",
+
+	"health" : 70,
+
+	"strength" : 70,
+
+	"defence" : 50,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 75
+}
+
+enemy_werewovles = {
+	"name" : "Werewovles",
+
+	"health" : 70,
+
+	"strength" : 40,
+
+	"defence" : 10,
+
+	"speed" : 50,
+
+	"reward" : "",
+
+	"money" : 50
+}
+
+enemy_hugeants = {
+	"name" : "Huge Ants",
+
+	"health" : 10,
+
+	"strength" : 20,
+
+	"defence" : 10,
+
+	"speed" : 50,
+
+	"reward" : "",
+
+	"money" : 45
+}
+
+enemy_evilmonk = {
+	"name" : "Evil Monk",
+
+	"health" : 20,
+
+	"strength" : 20,
+
+	"defence" : 10,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 30
+}
+
+enemy_centipede = {
+	"name" : "Vicious Centipede",
+
+	"health" : 40,
+
+	"strength" : 20,
+
+	"defence" : 20,
+
+	"speed" : 5,
+
+	"reward" : "",
+
+	"money" : 20
+}
+
+enemy_arachnoid = {
+	"name" : "Arachnoid",
+
+	"health" : 50,
+
+	"strength" : 10,
+
+	"defence" : 10,
+
+	"speed" : 30,
+
+	"reward" : "",
+
+	"money" : 30
+}
+
+enemy_serpent = {
+	"name" : "Serpent",
+
+	"health" : 30,
+
+	"strength" : 30,
+
+	"defence" : 10,
+
+	"speed" : 60,
+
+	"reward" : "",
+
+	"money" : 50
+}
+
+enemy_barghest = {
+	"name" : "Barghest",
+
+	"health" : 50,
+
+	"strength" : 10,
+
+	"defence" : 10,
+
+	"speed" : 30,
+
+	"reward" : "",
+
+	"money" : 30
+}
+
+enemy_worgen = {
+	"name" : "Worgen",
+
+	"health" : 60,
+
+	"strength" : 10,
+
+	"defence" : 30,
+
+	"speed" : 30,
+
+	"reward" : "",
+
+	"money" : 50
+}
+
+enemy_manticore = {
+	"name" : "Manticore",
+
+	"health" : 60,
+
+	"strength" : 40,
+
+	"defence" : 20,
+
+	"speed" : 50,
+
+	"reward" : "",
+
+	"money" : 100
+}
+
+enemy_possvillagers = {
+	"name" : "Possessed Villagers",
+
+	"health" : 50,
+
+	"strength" : 10,
+
+	"defence" : 10,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 20
+}
+
+enemy_darkelves = {
+	"name" : "Dark Elves",
+
+	"health" : 20,
+
+	"strength" : 20,
+
+	"defence" : 20,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 20
+}
+
+enemy_frogzard = {
+	"name" : "Frogzard",
+
+	"health" : 30,
+
+	"strength" : 10,
+
+	"defence" : 20,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 20
+}
+
+enemy_mantik = {
+	"name" : "Mantik",
+
+	"health" : 20,
+
+	"strength" : 10,
+
+	"defence" : 50,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 50
+}
+
+enemy_rat = {
+	"name" : "Rat",
+
+	"health" : 20,
+
+	"strength" : 20,
+
+	"defence" : 10,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 20
+}
+
+enemy_crab = {
+	"name" : "Poisonous Crab",
+
+	"health" : 20,
+
+	"strength" : 60,
+
+	"defence" : 10,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 40
+}
+
+enemy_giantcrab = {
+	"name" : "Giant Poisonous Crab",
+
+	"health" : 40,
+
+	"strength" : 80,
+
+	"defence" : 20,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 100
+}
+
+enemy_groupgoblin = {
+	"name" : "Group of Goblins",
+
+	"health" : 70,
+
+	"strength" : 60,
+
+	"defence" : 50,
+
+	"speed" : 30,
+
+	"reward" : "",
+
+	"money" : 100
+}
+
+enemy_twinserpent = {
+	"name" : "Twin-Headed Serpent",
+
+	"health" : 80,
+
+	"strength" : 60,
+
+	"defence" : 40,
+
+	"speed" : 70,
+
+	"reward" : "",
+
+	"money" : 100
+}
+
+enemy_phoenix = {
+	"name" : "Phoenix",
+
+	"health" : 20,
+
+	"strength" : 60,
+
+	"defence" : 10,
+
+	"speed" : 90,
+
+	"reward" : "",
+
+	"money" : 75
+}
+
+enemy_wraith = {
+	"name" : "Wraith",
+
+	"health" : 30,
+
+	"strength" : 60,
+
+	"defence" : 10,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 60
+}
+
+enemy_bugbear = {
+	"name" : "Wraith",
+
+	"health" : 30,
+
+	"strength" : 50,
+
+	"defence" : 40,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 70
+}
+
+enemy_giantorc = {
+	"name" : "Giant Orc",
+
+	"health" : 100,
+
+	"strength" : 90,
+
+	"defence" : 60,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 150
+}
+
+enemy_ettin = {
+	"name" : "Ettin",
+
+	"health" : 60,
+
+	"strength" : 20,
+
+	"defence" : 10,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 20
+}
+
+enemy_griffons = {
+	"name" : "Griffons",
+
+	"health" : 40,
+
+	"strength" : 40,
+
+	"defence" : 10,
+
+	"speed" : 20,
+
+	"reward" : "",
+
+	"money" : 50
+}
+
+enemy_lich = {
+	"name" : "Lich",
+
+	"health" : 20,
+
+	"strength" : 20,
+
+	"defence" : 10,
+
+	"speed" : 10,
+
+	"reward" : "",
+
+	"money" : 20
+}
+
+enemy_lichking = {
+	"name" : "Lich King",
+
+	"health" : 50,
+
+	"strength" : 40,
+
+	"defence" : 60,
+
+	"speed" : 30,
+
+	"reward" : "",
+
+	"money" : 50
+}
 enemy_list = {
 	1 : enemy_giantspider,
 	2 : enemy_goblin,
@@ -327,5 +851,38 @@ enemy_list = {
 	14 : enemy_beastmen,
 	15 : enemy_wildwolf,
 	16 : enemy_deadlyscorp,
-	17 : enemy_kraken
+	17 : enemy_kraken,
+	18 : enemy_ghoul,
+	19 : enemy_orc,
+	20 : enemy_cyclops,
+	21 : enemy_groupwolf,
+	22 : enemy_ogres,
+	23 : enemy_babyminotaurs,
+	24 : enemy_chiefminotaurs,
+	25 : enemy_werewovles,
+	26 : enemy_hugeants,
+	27 : enemy_evilmonk,
+	28 : enemy_centipede,
+	29 : enemy_arachnoid,
+	30 : enemy_serpent,
+	31 : enemy_barghest,
+	32 : enemy_worgen,
+	33 : enemy_manticore,
+	34 : enemy_possvillagers,
+	35 : enemy_darkelves,
+	36 : enemy_frogzard,
+	37 : enemy_mantik,
+	38 : enemy_rat,
+	39 : enemy_crab,
+	40 : enemy_giantcrab,
+	41 : enemy_groupgoblin,
+	42 : enemy_twinserpent,
+	43 : enemy_phoenix,
+	44 : enemy_wraith,
+	45 : enemy_bugbear,
+	46 : enemy_giantorc,
+	47 : enemy_ettin,
+	48 : enemy_griffons,
+	49 : enemy_lich,
+	50 : enemy_lichking
 }
--- map.py
+++ map.py
@@ -5,8 +5,8 @@ place_village = {
     "name": "Village",
 
     "description":
-    """A small town situated in the country of Azgorth. Home to mighty warriors and 
-the surrounded by vicious creatures, thirsty for blood.""",
+    """Ahead of you is a small village that you are able to enter. To the North lies a vast arena. To the east lies some sinister looking caves.
+    There is a large green lake to the south, and a large dark forest lies to the west.""",
 
     "exits": {"north": "Arena", "east": "Caves", "south": "Lake", "west": "Forest", "enter": "Shops"},
 
@@ -19,7 +19,11 @@ place_forest = {
     "name": "The Spooky Forest",
 
     "description":
-    """The spooky Forest, containing evil men and demons and shit.""",
+    """The dark forest is cold and the thick fog acts as a blanket across the mossy landscape. The ground beneath your feet is waterlogged and squishy.
+    Strange noises can be heard in the distance, the bushes russell behind you, when you turn round there is nothing there.
+    A small stream trickles down the valley towards the south.
+    Toward the north the forest starts to thicken as sunlight is almost completely lost amongst the trees,
+    more strange noises can be heard deeper into the forest""",
 
     "exits":  {"west": "Village", "north": "Deeper", "south": "Stream"},
 
@@ -32,7 +36,8 @@ place_deeper = {
     "name": "Deep Forest",
 
     "description":
-    """A place of enteral night where only the deadliest of monsters travel.""",
+    """The darkness of the forest limits your vision. The tree’s block the sunlight from above and a cold chill runs down your spine.
+    The strange noises that you heard before are getting louder the deeper you walk.""",
 
     "exits": {"south": "Forest"},
 
@@ -45,7 +50,9 @@ place_stream = {
     "name": "Forest Stream",
 
     "description":
-    """A small stream trickling through the Forest, most likely attracting animals.""",
+    """A small quiet clearing is surrounded by some tall trees rustling in the wind, a stream trickles quietly along the ground.
+    There are noises coming from the dark areas underneath the trees.
+    The clearing is a dead end, you can only go north back to the forest.""",
 
     "exits": {"north": "Forest"},
 
@@ -54,11 +61,64 @@ place_stream = {
     "battle": True
 }
 
+place_knight = {
+    "name": "Knight's Cave",
+
+    "description":
+    """Following the winding burnt stone walls of the cave you come to an opening with large bones scattered across the floor.
+    The bones look human. Looking deeper into the opening there is a large dragon skeleton in the centre of the cave.
+    Upon closer inspection of the skeleton you find a fallen knight in full royal armour.
+    The knight climbs to his feet and draws his sword.""",
+
+    "exits": {"west": "Caves"},
+
+    "items": [],
+
+    "battle": False
+}
+
+place_goblin = {
+    "name": "Goblin's Cave",
+
+    "description":
+    """You have crept into an opening deep in the cave which is lit by multiple torches across the walls of the cave.
+    You can make out creature like shadows on the walls from the opening of the cave.
+    Whilst you try and creep closer your sword drops and makes a large clanging noise on the floor, you scatter frantically to pick it up.
+    You look up from the cave floor to find a large goblin standing in front of you bearing a sharp pointed spear.
+    He looks ready to fight and you squeeze your weapon ready""",
+
+    "exits": {"south": "Caves"},
+
+    "items": [],
+
+    "battle": False
+}
+
+place_gutfeel = {
+    "name": "Gut Feeling",
+
+    "description":
+    """Your gut feeling is starting to feel a little bit off. As you walk deeper and deeper into the cave it gets even more dark.
+    You are now in complete darkness.
+    You should probably go back to to the main entrance but as soon as you turn round
+    a shadowed figure hits you across the head, knocking you unconscious.""",
+
+    "exits": {"north": "Caves"},
+
+    "items": [],
+
+    "battle": False
+}
+
 place_caves = {
     "name": "Forbidden Caves",
 
     "description":
-    """A series of caves twisting deep into the face of a mountain, home to who knows what creatures.""",
+    """The cave entrance is littered with bones of animals, the stench of death is strong in the air.
+    As you walk carefully into the cave entrance your eyes adjust to the darkness. The path is split into three.
+    There are noises of strange conversations coming from the northern exit, whatever is talking doesnt sound human.
+    The walls of the western exit look blackened as if they have been burnt. The southern path leads to a dark deeper cave,
+    but your gut feeling tells you that there is something of interest down there.""",
 
     "exits": {"west": "Village", "east": "Knight", "north": "Goblin", "south": "Gut Feeling"},
 
@@ -71,7 +131,10 @@ place_lake = {
     "name": "The Green Lake",
 
     "description":
-    """A greenish lake with the Kraken in. I know it's a shit temporary description just go with it.""",
+    """As you walk down the slippery banks toward the murky lake you notice that the water is not completely still.
+    Small bubbles rise and then pop on the surface of the murky green water.
+    As you descend the muddy banks of the lake your foot dislodges a stone and it falls into the lake.
+    To the south stands a small separate part of the lake, a sign with the words “Beware the beast” hangs loosely on a rock.""",
 
     "exits": {"north": "Village", "south": "Lair"},
 
@@ -84,7 +147,14 @@ place_shops = {
     "name": "Town Center",
 
     "description":
-    """The town center containing all the shops.""",
+    """The hustle and bustle of village life moves quickly around you. The cobblestone path beneath you leads to the village square.
+    Which has an ornate sculpted fountain at the center with children playing games beside it.
+    Stalls selling goods and fine foods are spread amongst the crowds of people bartering for the best deals.
+    The stone houses surrounding the square look tired and withered, some of  the thatched roofs are crumbling.
+    Many a strange person can be seen amongst the crowds, an armorsmith is loudly hammering away at a steel chest piece towards the east.
+    A shop at the far southern end of the square has weapons displayed in its windows, to the north there is a training ground for knights and warriors alike,
+    where groups of men are sparring.
+    Your small home sits quaintly at the west side of the village.""",
 
     "exits": {"north": "Gym", "east": "Armour", "south": "Weapons","west": "Home", "exit": "Village"},
 
@@ -96,7 +166,10 @@ place_shops = {
 place_arena = {
     "name": "Arena",
 
-    "description": """The place to prove your worth and reflect on the best.""",
+    "description": """As you approach the loud grounds of the arena you are met by two paths.
+    The path leading towards the east marked arena has deafening amounts of cheering and people are rushing towards the circular fighting grounds.
+    A booth at the foot of the path is marked ‘FIGHTERS WANTED BIG REWARD’.
+    The path to the west leads towards a huge marble wall with rows upon rows of names chiseled into it, it is titled ‘Hall Of Fame'.""",
 
     "exits": {"south": "Village", "west": "Battle", "east": "Leaderboards"},
 
@@ -109,7 +182,7 @@ place_leaderboard = {
     "name": "Leaderboard",
 
     "description":
-    """The place to look at the record books.""",
+    """This giant marble wall has rows upon rows of fallen warriors etched upon the marble.""",
 
     "exits": {"east": "Arena"},
 
@@ -122,7 +195,11 @@ place_battle = {
     "name": "Battlefield",
 
     "description":
-    """The place to fight warriors.""",
+    """The deafening noise of cheering onlookers spreads throughout the north. The arena walls are high and crowds start to flood into the fastly filling stands.
+    The King sits high above the crowds in his own personal box, the Queen by his side. As you walk through the dark tunnel leading to the main arena you notice
+    the sand is stained with blood of fallen heroes. Your opponents walk in at the opposite side of the arena and you meet them in the middle along with the
+    arena enforcers. The crowd silences as the King stands to speak.
+    After which you stare down your opponent before the fight begins.""",
 
     "exits": {"east": "Arena"},
 
@@ -135,9 +212,9 @@ place_gym = {
     "name": "Gym",
 
     "description":
-    """The place to get buff.""",
+    """The noise of angry men wrestling is met by the clang of swords clashing in the air. The sign reads above the fighting quarters “THE GYM.""",
 
-    "exits": {"west": "Shops"},
+    "exits": {"south": "Shops"},
 
     "items": [],
 
@@ -148,7 +225,9 @@ place_weapons = {
     "name": "Weapons",
 
     "description":
-    """The place to get weapons.""",
+    """As you enter the weaponsmith’s shop you are stunned by the sheer amount of shiny weaponry there is on offer. ‘Hello friend!’
+    the weaponsmith shouts from behind the counter,
+    ‘And what is it i can do for you on this fine day?’. You approach the fat bearded man.""",
 
     "exits": {"north": "Shops"},
 
@@ -161,9 +240,10 @@ place_armour = {
     "name": "Armour",
 
     "description":
-    """The place to get armour.""",
+    """The big burly armoursmith ignores you as you first walk in and continues to hammer away at a large steel sword.
+    You are amazed at the intricate armour displays that are on display. “What do you want?” the scarred armoursmith says.""",
 
-    "exits": {"south": "Shops"},
+    "exits": {"west": "Shops"},
 
     "items": [armour_halfhelm, armour_chain, armour_plate],
 
@@ -174,7 +254,7 @@ place_home = {
     "name": "Home",
 
     "description":
-    """The place you live, obviously.""",
+    """This is you warm humble home. The fire is burning brightly in the corner, with your shaggy coat sheep dog lying asleep besides it.""",
 
     "exits": {"east": "Shops"},
 
@@ -212,4 +292,7 @@ places = {
     "Deeper": place_deeper,
     "Lake": place_lake,
     "Lair": place_lair
+    "Gut": place_gutfeel
+    "Knight": place_knight
+    "Goblin": place_goblin
 }
